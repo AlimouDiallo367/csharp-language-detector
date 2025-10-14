d113b3bf864ed0bbbe7c2d17df094ffff57f1422
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-10-14 19:59:14 -0400

    Disable detection button when input is empty(HomeView): done

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TP2_DetectionLangue.ViewModels
 {
-    public class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged 
     {
         public BaseViewModel()
         {
             
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP2_DetectionLangue.ViewModels.Commands;
 
 namespace TP2_DetectionLangue.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        #region RelayCommands
+        public RelayCommand DetectCommand {  get; private set; }
+        #endregion
+
+        private string _textToDetect;
+        public string TextToDetect
+        {
+            get => _textToDetect;
+            set
+            {
+                _textToDetect = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public HomeViewModel()
+        {
+            DetectCommand = new RelayCommand(Detect, CanDetect);
+        }
+
+        private void Detect(object? obj)
+        {
+            // TODO ...
+        }
+
+        private bool CanDetect(object? obj)
+        {
+            return !string.IsNullOrWhiteSpace(_textToDetect);
+        }
     }
 }
