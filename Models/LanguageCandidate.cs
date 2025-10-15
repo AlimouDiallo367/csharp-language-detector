6784db26b494884763b263093029655034e8b9d1
Thierno Alimou Diallo <thalimoud66@gmail.com>
2025-10-15 14:09:27 -0400

    feat: Converting language code to full language name in HomeView's Combobox

--- Models/LanguageCandidate.cs
+++ Models/LanguageCandidate.cs
@@ -10,5 +10,22 @@ namespace TP2_DetectionLangue.Models
     {
         public string Language { get; set; }
         public double Score { get; set; }
+
+        public string FullLanguageName => LanguageCodeMap.ContainsKey(Language)
+            ? LanguageCodeMap[Language]
+            : Language;
+
+        private static readonly Dictionary<string, string> LanguageCodeMap = new()
+        {
+            {"fr", "FRENCH"},
+            {"en", "ENGLISH"},
+            {"es", "SPANISH"},
+            {"de", "GERMAN"},
+            {"it", "ITALIAN"},
+            {"pt", "PORTUGUESE"},
+            {"ja", "JAPANESE"},
+            {"zh", "CHINESE"},
+            // tu peux ajouter d'autres codes ISO que tu veux
+        };   
     }
 }
